49fece708d291ad6b6c776549a0c869b9ccb9d0e
Matt Gaidica <matt@gaidi.ca>
2015-05-18 14:19:10 -0400

    Old changes

--- util/run_saveHighResFigure.m
+++ util/run_saveHighResFigure.m
@@ -4,4 +4,4 @@ figpos=getpixelposition(h); %dont need to change anything here
 resolution=get(0,'ScreenPixelsPerInch'); %dont need to change anything here
 set(h,'paperunits','inches','papersize',figpos(3:4)/resolution,'paperposition',[0 0 figpos(3:4)/resolution]); 
 
-print(h,fullfile('/Users/mattgaidica/Dropbox/School/ChestekLab/KetaminePaper','plot_power_M1_90min'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
+print(h,fullfile('/Users/mattgaidica/Documents/Data/SkilledReaching/R0039','earlyLateMissSuccess_R0039'),'-dpng',['-r',num2str(rez)],'-opengl') %save file 
